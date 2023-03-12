c4738a2418f7a7092033f3119deebf8adbd421e0
shivaPeri <shivaperi47@gmail.com>
2023-03-12 19:33:40 -0400

    added surface normals

--- julia/raytrace.jl
+++ julia/raytrace.jl
@@ -6,6 +6,10 @@ Point3 = Array{Float64,1}
 Vec3 = Array{Float64,1}
 Color = Array{Float64,1}
 
+point3(x, y, z) = Point3([x,y,z])
+vec3(x, y, z) = Vec3([x,y,z])
+color(x, y, z) = Color([x,y,z])
+
 # Accessor methods
 x = v -> v[1]
 y = v -> v[2]
@@ -21,19 +25,31 @@ function at(ray::Ray, t)
 end
 
 
-# function hit_sphere(center, radius, r)
-#     oc = r.origin .- center
-#     a = dot(r.direction(), r.direction())
-#     b = 2.0 * dot(oc, r.direction())
-#     c = dot(oc, oc) - radius*radius
-#     discriminant = b*b - 4*a*c
-#     return (discriminant > 0);
-# end
+function hit_sphere(center, radius, r)
+    oc = r.origin .- center
+    a = dot(r.direction, r.direction)
+    b = 2.0 * dot(oc, r.direction)
+    c = dot(oc, oc) .- radius .* radius
+    discriminant = b .* b .- 4 .* a .* c
+
+    if discriminant < 0
+        return -1.0
+    else
+        return (-b - sqrt(discriminant) ) / (2.0 .* a)
+    end
+
+end
 
 function ray_color(ray)
+
+    t = hit_sphere(point3(0,0,-1), 0.5, ray)
+    if t > 0
+        N = normalize(at(ray, t) - vec3(0,0,-1))
+        return 0.5 .* color(x(N)+1, y(N)+1, z(N)+1)
+    end
     unit_direction = normalize(ray.direction)
-    t = 0.5 * (y(unit_direction) + 1.0);
-    return (1.0-t) * Color([1.0, 1.0, 1.0]) + t * Color([0.5, 0.7, 1.0]);
+    t = 0.5 * (y(unit_direction) + 1.0)
+    return (1.0-t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 end
 
 # Write the translated [0,255] value of each color component.
@@ -62,10 +78,10 @@ function main()
     viewport_width = aspect_ratio * viewport_height
     focal_length = 1.0
 
-    origin = Point3([0, 0, 0])
-    horizontal = Vec3([viewport_width, 0, 0])
-    vertical = Vec3([0, viewport_height, 0])
-    lower_left_corner = origin .- horizontal./2 .- vertical./2 .- Vec3([0, 0, focal_length])
+    origin = point3(0, 0, 0)
+    horizontal = vec3(viewport_width, 0, 0)
+    vertical = vec3(0, viewport_height, 0)
+    lower_left_corner = origin .- horizontal./2 .- vertical./2 .- vec3(0, 0, focal_length)
 
     # Render
 
