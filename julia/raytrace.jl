dce619ff606e9088273426491f72cdd734e983ff
shivaPeri <shivaperi47@gmail.com>
2023-03-12 16:46:04 -0400

    added image creation for juli

--- julia/raytrace.jl
+++ julia/raytrace.jl
@@ -1,5 +1,29 @@
 function main()
-    print("hello world; this might be a raytracer (probably not yet though)")
+
+    # Image
+
+    image_width = 256;
+    image_height = 256;
+
+    # Render
+
+    println("P3\n", image_width, " ", image_height, "\n255");
+
+    for j in reverse(0:(image_height-1))
+        for i in 0:(image_width-1)
+        
+            r = Float32(i) / Float32(image_width-1)
+            g = Float32(j) / Float32(image_height-1)
+            b = 0.25
+
+            ir = floor(Int, 255.999 * r)
+            ig = floor(Int, 255.999 * g)
+            ib = floor(Int, 255.999 * b)
+
+            println(ir, " ", ig, " ", ib)
+        end
+    end
+
 end
 
 main()
