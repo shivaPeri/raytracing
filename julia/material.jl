fc6d9e3be1b5ff9eb788fc8e5e89ddcebc9a6e31
shivaPeri <shivaperi47@gmail.com>
2023-03-16 00:19:10 -0400

    created Lambertian and Metal material

--- julia/material.jl
+++ julia/material.jl
@@ -1,12 +1,44 @@
-# module Material
+module MaterialModule
+
+export Lambertian, Metal
 
 include("utils.jl")
 using .Utils
+include("rays.jl")
+using .Rays
 
-function test()
-    x = vec3(0,0,0)
-    print(x)
+using LinearAlgebra
+
+
+# a Material is a function which
+# returns a scatter function based on input(s)
+
+function Lambertian(albedo::Color)::Function
+
+    function scatter(r_in, rec, attenuation::Color, scattered)
+        scatter_dir = rec.normal + random_unit_vector()
+        if near_zero(scatter_dir)
+            scatter_dir = rec.normal
+        end
+        
+        scattered = Ray(rec.point, scatter_dir)
+        attenuation = albedo
+        return true
+    end
+
+    return scatter
 end
 
-test()
-# end
+function Metal(albedo::Color)::Function
+
+    function scatter(r_in, rec, attenuation::Color, scattered)
+        reflected = reflect(normalize(r_in.direction), rec.normal)        
+        scattered = Ray(rec.point, reflected)
+        attenuation = albedo
+        return dot(scattered.direction, rec.normal) > 0
+    end
+
+    return scatter
+end
+
+end
