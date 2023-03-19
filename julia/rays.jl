92365fe56c2a0ef579d6c0212e711fb63e3672c0
shivaPeri <shivaperi47@gmail.com>
2023-03-19 17:36:07 -0400

    simplified export types

--- julia/rays.jl
+++ julia/rays.jl
@@ -14,16 +14,10 @@ end
 
 Ray() = Ray(point3(), vec3())
 
-function at(ray::Ray, t)
+function at(ray, t)
     return ray.origin .+ t * ray.direction
 end
 
-# makes sure surface normals always point outwards
-function set_face_normal(ray::Ray, outward_normal::Vec3)::Vec3
-    front_face = dot(ray.direction, outward_normal) < 0
-    return front_face ? outward_normal : -outward_normal
-end
-
-export Ray, at, set_face_normal
+export Ray, at
 
 end
