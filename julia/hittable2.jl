3abc3177f2b75a20714bc791f87da0a58cf6c048
shivaPeri <shivaperi47@gmail.com>
2023-03-18 21:09:17 -0400

    moving more functionality into hittable refactor file

--- julia/hittable2.jl
+++ julia/hittable2.jl
@@ -40,4 +40,54 @@ struct Hittable_List <: Hittable
     objects::Vector{Hittable}
 end
 
+
+# Sphere Class and associated methods
+struct Sphere <: Hittable
+    center::Point3
+    radius::Float64
+    mat::Function
+end
+
+function hit(sphere::Sphere, ray::Ray, t_min::Float32, t_max::Float32)::Hit
+    oc = ray.origin .- sphere.center
+    a = norm(ray.direction)^2
+    half_b = dot(oc, ray.direction)
+    c = norm(oc)^2 - sphere.radius * sphere.radius
+    
+    discriminant = half_b * half_b - a * c
+    if discriminant < 0 return Hit() end
+    sqrtd = sqrt(discriminant)
+
+    # Find the nearest root that lies in the acceptable range.
+    root = (-half_b - sqrtd) / a
+    if (root < t_min || t_max < root)
+        root = (-half_b + sqrtd) / a
+        if (root < t_min || t_max < root) 
+            return Hit()
+        end
+    end
+
+    point = at(ray, root)
+    outward_normal = (point .- sphere.center) ./ sphere.radius
+    normal = set_face_normal(ray, outward_normal)
+    rec = Hit(point, normal, Float32(root),sphere.mat)
+    return rec 
+end
+
+function hit(world::Hittable_List, ray::Ray, t_min::Float32, t_max::Float32)::Hit
+
+    rec = Hit()
+    closest_so_far = t_max
+
+    for object in world.objects
+        tmp = hit(object, ray, t_min, closest_so_far)
+        if tmp.val != nothing
+            rec = tmp
+            closest_so_far = rec.val.t
+        end
+    end
+
+    return rec
+end
+
 end
