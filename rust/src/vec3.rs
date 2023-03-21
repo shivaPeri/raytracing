ac674b14ec9a1c9c12912147430e0cb6bac27229
shivaPeri <shivaperi47@gmail.com>
2023-03-20 21:21:39 -0400

    restructred vec struct

--- rust/src/vec3.rs
+++ rust/src/vec3.rs
@@ -1,29 +1,40 @@
-use rand::Rng;
-use std::ops::{Add,Sub,Mul,Div,Neg}
-
-
+// use rand::Rng;
+use std::ops::{Add, Div, Mul, Neg, Sub};
 
 #[derive(Debug, Clone, Copy)]
-struct Vec3([f32; 3]);
-use Vec3 as Point3;
-use Vec3 as Color;
+pub struct Vec3 {
+    x: f32,
+    y: f32,
+    z: f32,
+}
 
-impl Vec3{
-    pub fn new() -> Self {
-        Vec3([0,0,0])
+pub type Point3 = Vec3;
+pub type Color = Vec3;
+
+impl Vec3 {
+    pub fn new(x: f32, y: f32, z: f32) -> Self {
+        Vec3 { x, y, z }
+    }
+
+    pub fn zero() -> Vec3 {
+        Vec3 {
+            x: 0.,
+            y: 0.,
+            z: 0.,
+        }
     }
 
     // Accessor methods
     pub fn x(&self) -> f32 {
-        self[0]
+        self.x
     }
-    
+
     pub fn y(&self) -> f32 {
-        self[1]
+        self.y
     }
-    
+
     pub fn z(&self) -> f32 {
-        self[2]
+        self.z
     }
 
     pub fn length(&self) -> f32 {
@@ -31,33 +42,40 @@ impl Vec3{
     }
 
     pub fn length_squared(&self) -> f32 {
-        self.[0]*self[0] + self[1]*self[1] + self[2]*self[2]
+        self.x * self.x + self.y * self.y + self.z * self.z
     }
 
     // other methods
     pub fn dot(&self, other: &Vec3) -> f32 {
-        self.x() * other.x() + self.y() * other.y() + self.z() * other.z()
-    }
-    
-    pub fn cross(&self, other: &Vec3) -> Vec3 {
-        Vec3{[
-            self.y() * other.z() - self.z() * other.y(),
-            self.z() * other.x() - self.x() * other.z(),
-            self.x() * other.y() - self.y() * other.x()
-        ]}
-    }
-    
-    pub fn unit_vector(&self) -> Vec3 {
-        let length = self.length();
-        Vec3{[ self.x() / length, self.y() / length, self.z() / length ]}
+        self.x * other.x() + self.y * other.y() + self.z * other.z()
     }
 
-    // pub fn normalize(&self) -> Vec3 {
-        
-    // }
-    
+    pub fn cross(&self, other: &Vec3) -> Vec3 {
+        Vec3 {
+            x: self.y * other.z() - self.z * other.y(),
+            y: self.z * other.x() - self.x * other.z(),
+            z: self.x * other.y() - self.y * other.x(),
+        }
+    }
+
+    pub fn unit_vector(&self) -> Vec3 {
+        let length = self.length();
+        Vec3 {
+            x: self.x / length,
+            y: self.y / length,
+            z: self.z / length,
+        }
+    }
+
+    pub fn normalize(&mut self) {
+        let length = self.length();
+        self.x /= length;
+        self.y /= length;
+        self.z /= length;
+    }
+
     pub fn near_zero(&self) -> bool {
-        false   
+        false
     }
 }
 
@@ -65,46 +83,101 @@ impl Add for Vec3 {
     type Output = Self;
 
     fn add(self, other: Self) -> Self {
-        Self{[
-            self.x() + other.x(),
-            self.y() + other.y(),
-            self.z() + other.z()
-        ]}
-    }     
+        Self {
+            x: self.x + other.x(),
+            y: self.y + other.y(),
+            z: self.z + other.z(),
+        }
+    }
 }
 
 impl Sub for Vec3 {
     type Output = Self;
 
     fn sub(self, other: Self) -> Self {
-        Self{[
-            self.x() - other.x(),
-            self.y() - other.y(),
-            self.z() - other.z()
-        ]}
-    }     
+        Self {
+            x: self.x - other.x(),
+            y: self.y - other.y(),
+            z: self.z - other.z(),
+        }
+    }
 }
 
 impl Neg for Vec3 {
     type Output = Self;
 
-    fn neg(self, other: Self) -> Self {
-        Self{[
-            -self.x(),
-            -self.y(),
-            -self.z()
-        ]}
-    }     
+    fn neg(self) -> Self {
+        Self {
+            x: -self.x,
+            y: -self.y,
+            z: -self.z,
+        }
+    }
 }
 
-impl Mul<f64> for Vec3 {
+impl Mul<f32> for Vec3 {
     type Output = Self;
 
-    fn mul(self, other: f64) -> Self {
-        Self{[
-            self.x() * other,
-            self.y() * other,
-            self.z() * other,
-        ]}
+    fn mul(self, other: f32) -> Self {
+        Self {
+            x: self.x * other,
+            y: self.y * other,
+            z: self.z * other,
+        }
     }
+}
+
+impl Mul<Vec3> for Vec3 {
+    type Output = Self;
+
+    fn mul(self, other: Vec3) -> Self {
+        Self {
+            x: self.x * other.x(),
+            y: self.y * other.y(),
+            z: self.z * other.z(),
+        }
+    }
+}
+
+impl Div<f32> for Vec3 {
+    type Output = Self;
+
+    fn div(self, other: f32) -> Self {
+        Self {
+            x: self.x / other,
+            y: self.y / other,
+            z: self.z / other,
+        }
+    }
+}
+
+impl Div<Vec3> for Vec3 {
+    type Output = Self;
+
+    fn div(self, other: Vec3) -> Self {
+        Self {
+            x: self.x / other.x(),
+            y: self.y / other.y(),
+            z: self.z / other.z(),
+        }
+    }
+}
+
+///////////////////////////// TESTS
+
+#[test]
+fn test_new() {
+    let p = Vec3 {
+        x: 1.,
+        y: 2.,
+        z: 3.,
+    };
+    assert_eq!(p.x(), 1.);
+    assert_eq!(p.y(), 2.);
+    assert_eq!(p.z(), 3.);
+
+    let q = Vec3::new(1., 2., 3.);
+    assert_eq!(q.x(), 1.);
+    assert_eq!(q.y(), 2.);
+    assert_eq!(q.z(), 3.);
 }
